27a7cd449d44f540023289ee44fbca5045426bfa
Faizal Amiruddin <faizal@wowrack.com>
2021-11-26 19:44:43 +0700

    change configuration

--- webpack.common.js
+++ webpack.common.js
@@ -11,32 +11,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-    ],
-  },
   plugins: [
     new Dotenv(),
     new HtmlWebpackPlugin({
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,32 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
   },
