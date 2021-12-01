0f8290d8775186e1c8a8f63717e6016b1e745694
Faizal Amiruddin <faizal@wowrack.com>
2021-12-02 04:56:43 +0700

    e2e testing

--- e2e/addAReview.spec.js
+++ e2e/addAReview.spec.js
@@ -1,10 +1,11 @@
 Feature('Add a Review');
 
-Scenario('Open detail and add a valid review', ({ I }) => {
+Before(({ I }) => {
   I.amOnPage('/#/');
-
   I.click(locate('#restaurants-container restaurant-card .restaurant-detail-link').first());
+});
 
+Scenario('Open detail and add a valid review', ({ I }) => {
   const reviewMessage = `Review from codecept on ${new Date().toLocaleString()}.`;
   I.fillField('Name', 'Codecept');
   I.fillField('Review', reviewMessage);
@@ -12,4 +13,24 @@ Scenario('Open detail and add a valid review', ({ I }) => {
 
   I.see('Codecept', '.review-box .reviewer-name');
   I.see(reviewMessage, '.review-box .review-message');
+});
+
+Scenario('Open detail and add a invalid review', ({ I }) => {
+  I.fillField('Name', '');
+  I.fillField('Review', '');
+  I.click('Post');
+
+  I.see('Failed to post review.', '#error-review.block');
+});
+
+Scenario('Open detail and add a review while offline', ({ I }) => {
+  I.usePuppeteerTo('emulate offline mode', async ({ page, browser }) => {
+    await page.setOfflineMode(true);
+  });
+
+  I.fillField('Name', 'Codecept');
+  I.fillField('Review', 'Send review while offline.');
+  I.click('Post');
+
+  I.see('You are offline.', '#error-review.block');
 });
